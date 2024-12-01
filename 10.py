4c34805258a0b64f74ee405f2120ba09f0f4f0ad
Ajeep <ajeep8@gmail.com>
2024-12-01 19:39:58 +0800

    10课

---
+++ 10.py
@@ -0,0 +1,33 @@
+from gpiozero import Button
+from gpiozero import LED
+from time import sleep
+from signal import pause
+
+makerobo_ReedPin = Button(17)
+p_R =LED(18)
+p_G = LED(27)
+
+def pressed():
+    print('***** makerobo Detected Magnetic Material! *')
+    p_R.on()
+    p_G.off()
+
+def released():
+    p_R.off()
+    p_G.on()
+
+makerobo_ReedPin.when_pressed = pressed
+makerobo_ReedPin.when_released = released
+
+def makerobo_loop():
+    pause()
+
+def makerobo_destroy():
+    p_R.close()
+    p_G.close()
+
+if __name__=='__main__':
+    try:
+        makerobo_loop()
+    except KeyboardInterrupt:
+        makerobo_destroy()
